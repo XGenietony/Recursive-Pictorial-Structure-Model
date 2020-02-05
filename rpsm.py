2d2c7c9d08c4e36010ec173ebe87cd242bf594fa
Saman Vafadar <saman.vafadar@yahoo.com>
2020-02-05 16:07:26 +0100

    computation of pose3d based on softmax instead of max computation of pairwise based on gaussian distribution added comments

--- rpsm.py
+++ rpsm.py
@@ -1,13 +1,21 @@
-# Hello
-
 import pickle
 import numpy as np
 import torch
-from bisect import bisect_left, bisect_right 
-
+from tabulate import tabulate
 from body import HumanBody
 
 def infer(unary, pairwise, body):
+    """
+    Args:
+        unary:
+
+        pairwise:
+
+        body:
+    
+    Return:
+        pose3d_as_cube_idx, states_of_all_joints
+    """
     skeleton = body.skeleton
     skeleton_sorted_by_level = body.skeleton_sorted_by_level
 
@@ -53,7 +61,19 @@ def infer(unary, pairwise, body):
                 queue.append([joint_idx, cube_idx])
 
     pose3d_as_cube_idx.sort()
-    return pose3d_as_cube_idx
+    return pose3d_as_cube_idx, states_of_all_joints
+
+def get_loc_from_states_of_joints(states_of_all_joints, grids):
+    njoints = len(states_of_all_joints)
+    pose3d = torch.zeros((njoints, 3))
+    for joint_idx in range(njoints):
+        energy = torch.nn.functional.softmax(states_of_all_joints[joint_idx]['Energy'].view(-1,1), dim=0)
+        # energy = states_of_all_joints[joint_idx]['Energy']
+        # energy = (energy/(torch.sum(energy)+1e-8)).view(-1,1)
+        energy = energy.expand_as(grids[joint_idx])
+        pose3d[joint_idx] = torch.sum(energy*grids[joint_idx], dim=0)
+   
+    return pose3d
 
 def get_loc_from_cube_idx(grid, pose3d_as_cube_idx):
     """
@@ -100,31 +120,91 @@ def pdist2(x, y):
     dist = torch.pairwise_distance(xtile, ytile)
     return dist.view(n, m)
 
+def probabilty_function(x, sigma, mu):
+    return np.exp((-1/2)*(((x-mu)/sigma)**2))/(sigma*np.sqrt(2*np.pi))
+
 def compute_pairwise(skeleton, limb_length_gt, grids, tolerance):
+    """
+    Args:
+        skeleton: n lists of dicts of {'childern:'[], 'idx':[], 'level':[], 'name':[], 'parent':[]}
+            -n: number of joints
+        
+        limb_length_gt: a dict of k keys
+            -k: number of child-parent connections (16 connections - (lelbow, lwrist), (pelvis, lhip), ...)
+
+        grids: n lists of tensors of size (nbins * 3)
+            -n: number of joints
+            -nbins: number of bins in the grid
+        
+        tolerance: a single value
+    Return:
+        pairwise: a dict of k keys of n * n values
+            -k: number of child-parent connections (16 connections - (lelbow, lwrist), (pelvis, lhip), ...)
+            -n: number of bins in grid
+    """
     pairwise = {}
     for node in skeleton:
         current = node['idx']
         children = node['children']
         for child in children:
+            # length of the segments based on the ground truth data
             expect_length = limb_length_gt[(current, child)]
-            distance = pdist2(grids[current], grids[child]) + 1e-9
-            pairwise[(current, child)] = (torch.abs(distance - expect_length) < tolerance).float()
+
+            # compute the distance between the child grid and the parent grid
+            distance = pdist2(grids[current], grids[child])
+            
+            # compute the pairwise probability term using a gaussian distribution
+            probability = probabilty_function(distance - expect_length, tolerance, 0)
+
+            # normalize the probabilities
+            probability = (probability/torch.max(probability + 1e-9))
+
+            pairwise[(current, child)] = probability
+
+            # pairwise[(current, child)] = (torch.abs(distance - expect_length) < tolerance).float()
     return pairwise
 
 def compute_grid(boxSize, boxCenter, nBins, device=None):
+    
     grid1D = torch.linspace(-boxSize / 2, boxSize / 2, nBins, device=device)
+
     gridx, gridy, gridz = torch.meshgrid(
         grid1D + boxCenter[0],
         grid1D + boxCenter[1],
-        grid1D + boxCenter[2],
-    )
+        grid1D + boxCenter[2],)
+    
     gridx = gridx.contiguous().view(-1, 1)
     gridy = gridy.contiguous().view(-1, 1)
     gridz = gridz.contiguous().view(-1, 1)
     grid = torch.cat([gridx, gridy, gridz], dim=1)
+
     return grid
 
 def compute_unary_term(volumes_pred, coord_volumes_pred, grids):
+    """
+    compute the probability of the joints existence in the bins of grid
+
+    Args:
+        volumes_pred: array of size (n * i * j * k)
+            -n: number of joints
+            -i: length
+            -j: width
+            -k: height
+        
+        coord_volumes_pred: array of size (i * j * k * 3)
+            -i: length
+            -j: width
+            -k: height
+
+        grids: n lists of tensors of size (nbins * 3)
+            -n: number of joints
+            -nbins: number of bins in the grid
+    
+    Returns:
+        all_unary_list: a list of tensors of size nbins
+
+    """
+    # initialization of parameters
     njoints = volumes_pred.shape[0]
     nbins = grids[0].shape[0]
 
@@ -137,64 +217,73 @@ def compute_unary_term(volumes_pred, coord_volumes_pred, grids):
     idxs = idxs.reshape((-1, 3))
 
     index = np.ones((njoints, nbins, 27, 3), dtype=np.int16)
-    dist  = np.zeros((njoints, nbins, 27), dtype=np.float32)
+    dist  = np.zeros((njoints, nbins, 27), dtype=np.float64)
     ndist = np.zeros_like(dist)
     all_unary_list = []
-    all_unary = np.zeros((njoints,nbins), dtype=np.float32)
-    unary_temp = np.zeros((njoints,nbins,27), dtype=np.float32)
+    all_unary = np.zeros((njoints,nbins), dtype=np.float64)
+    unary_temp = np.zeros((njoints,nbins,27), dtype=np.float64)
 
     for i in range(njoints):
         for j, grid  in enumerate(grids[i]):
+            # find the index of closest coordinate in the coord_volumes_pred to the bins of grid
             index[i][j][0][0] = np.argmin(np.abs(xxx - grid[0].numpy()))
             index[i][j][0][1] = np.argmin(np.abs(yyy - grid[1].numpy()))
             index[i][j][0][2] = np.argmin(np.abs(zzz - grid[2].numpy()))
 
+            # determine the indices of neighboring bins of the located bin in coord_volumes_pred
             index[i][j][1:] = index[i][j][0]*index[i][j][1:]
             index[i][j] = index[i][j] + idxs
 
+            # compute the distance of the bin of grid to the determined bins of coord_volumes_pred
             for k in range(index.shape[2]):
                 dist[i][j][k] = np.linalg.norm(coord_volumes_pred[index[i][j][k][0], index[i][j][k][1], index[i][j][k][2]] - grid.numpy())
             
+            # normalize the distances
             ndist[i][j] = (1/dist[i][j])/np.sum(1/dist[i][j])
 
+            # compute the unary term, based on the normalized distances of the bin of grid from the bins of the coord_volumes_pred
             for k in range(index.shape[2]):
                 unary_temp[i][j][k] = ndist[i][j][k]*volumes_pred[i, index[i][j][k][0], index[i][j][k][1], index[i][j][k][2]]
             all_unary[i,j] = np.sum(unary_temp[i][j])
 
         all_unary_list.append(torch.from_numpy(all_unary[i]).float())
-            
+   
     return all_unary_list
 
 def rpsm(volumes_pred, keypoints_3d_pred, keypoints_3d_gt, coord_volumes_pred):
+    # initialization
     rec_depth = 8
     njoints = keypoints_3d_gt.shape[0]
     nbins = 2
     device = torch.device('cpu')
 
+    # computation of the limb lengths ground truth
     body = HumanBody()
     skeleton = body.skeleton
     limb_length_gt = compute_limb_length(keypoints_3d_gt, skeleton)
 
     grid_size = np.linalg.norm(coord_volumes_pred[0][0][0] - coord_volumes_pred[0][0][1]).round()
 
-    # pose3d = keypoints_3d_pred
-    pose3d = np.zeros((njoints,3), dtype=np.float32)
-    for i in range(njoints):
-        ind = np.unravel_index(np.argmax(volumes_pred[i], axis=None), volumes_pred[i].shape)
-        pose3d[i] = coord_volumes_pred[ind[0], ind[1], ind[2]]
-
+    pose3d = keypoints_3d_pred
+    # pose3d = np.zeros((njoints,3), dtype=np.float64)
+    # for i in range(njoints):
+    #     ind = np.unravel_index(np.argmax(volumes_pred[i], axis=None), volumes_pred[i].shape)
+    #     pose3d[i] = coord_volumes_pred[ind[0], ind[1], ind[2]]
+    
     for k in range(rec_depth):
-        grid_size = grid_size / nbins
         grids = []
-        tolerance = np.multiply(np.sqrt(2), grid_size)
+        tolerance = 0.1*np.multiply(np.sqrt(2), grid_size)
 
         for i in range(njoints):
             grids.append(compute_grid(grid_size, pose3d[i], nbins, device=device))
 
-        pairwise    = compute_pairwise(skeleton, limb_length_gt, grids, tolerance)
-        unary       = compute_unary_term(volumes_pred, coord_volumes_pred, grids)
-        pose3d_cube = infer(unary, pairwise, body)
-        pose3d = get_loc_from_cube_idx(grids, pose3d_cube)
+        pairwise = compute_pairwise(skeleton, limb_length_gt, grids, tolerance)
+        unary = compute_unary_term(volumes_pred, coord_volumes_pred, grids)
+        pose3d_cube, states_of_all_joints = infer(unary, pairwise, body)
+        pose3d = get_loc_from_states_of_joints(states_of_all_joints, grids)
+        # pose3d = get_loc_from_cube_idx(grids, pose3d_cube)
+
+        grid_size = grid_size / 2
     
     return pose3d
 
@@ -203,6 +292,7 @@ if __name__=='__main__':
         data = pickle.load(dict_data)
 
 
+    mpjpe_null = []
     batch_size = 10
     for i in range(batch_size):
         coord_volumes_pred = data['coord_volumes_pred'][i]
@@ -210,9 +300,14 @@ if __name__=='__main__':
         keypoints_3d_pred = data['keypoints_3d_pred'][i]
         volumes_pred = data['volumes_pred'][i]
 
-        volumes_pred = (volumes_pred/np.max(volumes_pred))*5 + 5
+        # Scale the volumes_pred (between 0 - 10) which will be used for computation of unary potential
+        volumes_pred = (volumes_pred/np.max(volumes_pred))*5
 
         pose3d = rpsm(volumes_pred, keypoints_3d_pred, keypoints_3d_gt, coord_volumes_pred).numpy()
+
+        mpjpe_pred = (np.sqrt(np.sum(((keypoints_3d_gt) - (pose3d))**2, axis=1)))
+        mpjpe_pose = (np.sqrt(np.sum(((keypoints_3d_gt) - (keypoints_3d_pred))**2, axis=1)))
+        mpjpe_null.append(np.sqrt(np.sum(((pose3d) - (keypoints_3d_pred))**2, axis=1)))
 
         mpjpe1 = np.mean(np.sqrt(np.sum(((keypoints_3d_gt - keypoints_3d_gt[6]) - (pose3d-pose3d[6]))**2, axis=1)))
         mpjpe2 = np.mean(np.sqrt(np.sum(((keypoints_3d_gt - keypoints_3d_gt[6]) - (keypoints_3d_pred-keypoints_3d_pred[6]))**2, axis=1)))
@@ -220,4 +315,15 @@ if __name__=='__main__':
         mpjpe3 = (np.sqrt(np.sum(((keypoints_3d_gt[6]) - (pose3d[6]))**2)))
         mpjpe4 = (np.sqrt(np.sum(((keypoints_3d_gt[6]) - (keypoints_3d_pred[6]))**2)))
 
-        print(mpjpe1, mpjpe2, '          ', mpjpe3, mpjpe4)
+        mpjpe5 = np.mean(np.sqrt(np.sum((keypoints_3d_gt - pose3d)**2, axis=1)))
+        mpjpe6 = np.mean(np.sqrt(np.sum((keypoints_3d_gt - keypoints_3d_pred)**2, axis=1)))
+        
+        print(mpjpe1, mpjpe2, '          ', mpjpe3, mpjpe4, '          ', mpjpe5, mpjpe6)
+        
+        # body = HumanBody()
+        # skeleton = body.skeleton
+        # header = []
+        # for i in range(17):
+        #     header.append(skeleton[i]['name'])
+        # table = [[mpjpe_pose.reshape(1,-1)],[mpjpe_pred.reshape(1,-1)]]
+        # print(tabulate(table, header, tablefmt='orgtbl'))
